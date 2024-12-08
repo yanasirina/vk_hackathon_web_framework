d05d5b818ad7f3d7ca711bf0dece2f854d98d52f
yanasirina <yanasirina0@yandex.ru>
2024-12-08 14:08:38 +0300

    добавила enum для content_type

--- web/responses.py
+++ web/responses.py
@@ -1,6 +1,12 @@
-from webob import Response as _Response
+from enum import Enum
 
+from webob import Response as _Response
 from jinja2 import Environment, FileSystemLoader
+
+
+class ContentType(Enum):
+    JSON = "application/json"
+    HTML = "text/html"
 
 
 class Response(_Response):
@@ -11,7 +17,7 @@ class Response(_Response):
 
 class JsonResponse(Response):
     def __init__(self, body: dict, *args, **kwargs):
-        super().__init__(*args, json_body=body, content_type='application/json', **kwargs)
+        super().__init__(*args, json_body=body, content_type=ContentType.JSON.value, **kwargs)
 
 
 class HTMLResponse(Response):
@@ -19,4 +25,4 @@ class HTMLResponse(Response):
         env = Environment(loader=FileSystemLoader(''))
         template = env.get_template(template_path)
         rendered_html = template.render(context)
-        super().__init__(*args, body=rendered_html, content_type='text/html', **kwargs)
+        super().__init__(*args, body=rendered_html, content_type=ContentType.HTML.value, **kwargs)
