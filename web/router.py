90aae045ca99e5fa4ea34584851ecd2bc76b0143
Daniil Mironenko <dan.mironenko@gmail.com>
2024-12-08 14:00:44 +0300

    integrate router and middlewares

--- web/router.py
+++ web/router.py
@@ -1,15 +1,23 @@
+import sys
+from typing import List, Optional
+
 from webob import Request, Response
 from webob.exc import HTTPNotFound, HTTPInternalServerError
+
+from .middleware import Middleware
 
 
 class Router:
     def __init__(self):
         self.routes = {}
-        self.not_found_handler = HTTPNotFound
+        self.not_found_handler = None
 
-    def route(self, path):
+    def route(self, path, middlewares: Optional[List[Middleware]] = None):
+        if middlewares is None:
+            middlewares = []
+
         def decorator(func):
-            self.routes[path] = func
+            self.routes[path] = self._apply_middlewares(func, middlewares)
             return func
 
         return decorator
@@ -18,16 +26,28 @@ class Router:
         self.not_found_handler = func
         return func
 
-    def __call__(self, environ, start_response) -> Response:
+    def __call__(self, environ, start_response):
         request = Request(environ)
+        response = Response()
 
         handler = self.routes.get(request.path_info)
         if handler:
             try:
                 response = handler(request)
-            except Exception:
+            except Exception as err:
+                print(err, file=sys.stdout)
                 response = HTTPInternalServerError()
         else:
-            response = self.not_found_handler(request)
+            if self.not_found_handler:
+                self.not_found_handler(request, response)
+            else:
+                response = HTTPNotFound()
 
         return response(environ, start_response)
+
+    @staticmethod
+    def _apply_middlewares(handler, middlewares):
+        for middleware in reversed(middlewares):
+            handler = middleware(handler)
+
+        return handler
