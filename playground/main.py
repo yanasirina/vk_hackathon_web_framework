b304c031363dc311c2a44cc5969d99002e1e124f
yanasirina <yanasirina0@yandex.ru>
2024-12-08 13:43:28 +0300

    прикрутила jinja2

--- playground/main.py
+++ playground/main.py
@@ -1,21 +1,31 @@
+import datetime
 import os
 
 import web
-from web import JsonResponse
+import web.responses
 
 
 app = web.Router()
 
 
+@app.route('/main')
+def html_example(_request):
+    response = web.responses.HTMLResponse(
+        template_path='templates/index.html',
+        context={'today_date': datetime.date.today()}
+    )
+    return response
+
+
 @app.route('/hello')
 def json_example(_request):
-    response = JsonResponse({'message': 'hello, world!'})
+    response = web.responses.JsonResponse({'message': 'hello, world!'})
     return response
 
 
 @app.not_found
 def custom_404(_request):
-    response = JsonResponse({'error': 'route not found'})
+    response = web.responses.JsonResponse({'error': 'route not found'})
     return response
 
 
--- web/responses.py
+++ web/responses.py
@@ -1,5 +1,7 @@
 from webob import Response as _Response
 
+from jinja2 import Environment, FileSystemLoader
+
 
 class Response(_Response):
     """
@@ -9,5 +11,17 @@ class Response(_Response):
 
 class JsonResponse(Response):
     def __init__(self, body: dict, *args, **kwargs):
-        super().__init__(*args, json_body=body, **kwargs)
+        super().__init__(json_body=body, *args, **kwargs)
         self.content_type = 'application/json'
+
+
+class HTMLResponse(Response):
+    def __init__(self, template_path: str, context: dict = None, *args, **kwargs):
+        context = context or {}
+
+        env = Environment(loader=FileSystemLoader(''))
+        template = env.get_template(template_path)
+        rendered_html = template.render(context)
+
+        super().__init__(body=rendered_html, *args, **kwargs)
+        self.content_type = 'text/html'
