4edf52a6b5b81be5f191d77abb99b0231682e3a1
Daniil Mironenko <dan.mironenko@gmail.com>
2024-12-08 15:10:34 +0300

    fix middleware

--- web/middleware.py
+++ web/middleware.py
@@ -1,19 +1,15 @@
-from abc import ABC, abstractmethod
-
 from webob import Request, Response
 
 
-class Middleware(ABC):
+class Middleware:
     def __init__(self, handler) -> None:
         self.handler = handler
 
-    @abstractmethod
     def before(self, request: Request) -> None:
         pass
 
-    @abstractmethod
     def after(self, request: Request, response: Response) -> Response:
-        pass
+        return response
 
     def __call__(self, request: Request) -> Response:
         self.before(request)
