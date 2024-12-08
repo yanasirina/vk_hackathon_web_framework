5c7918697662d6f7f318ed5706a7a1e7485feedc
Khorunzhiy Ilya <khorunzhiy.ib@gmail.com>
2024-12-08 13:55:07 +0300

    add routes

--- web/router.py
+++ web/router.py
@@ -9,42 +9,42 @@ class Router:
         self.routes = {}
         self.not_found_handler = HTTPNotFound
 
-    def add_route(self, method, path, func):
+    def _add_route(self, method, path, func):
         if path not in self.routes:
             self.routes[path] = {}
         self.routes[path][method] = func
 
     def get(self, path):
         def decorator(func):
-            self.add_route(HTTPMethod.GET, path, func)
+            self._add_route(HTTPMethod.GET, path, func)
             return func
 
         return decorator
 
     def post(self, path):
         def decorator(func):
-            self.add_route(HTTPMethod.POST, path, func)
+            self._add_route(HTTPMethod.POST, path, func)
             return func
 
         return decorator
 
     def put(self, path):
         def decorator(func):
-            self.add_route(HTTPMethod.PUT, path, func)
+            self._add_route(HTTPMethod.PUT, path, func)
             return func
 
         return decorator
 
     def patch(self, path):
         def decorator(func):
-            self.add_route(HTTPMethod.PATCH, path, func)
+            self._add_route(HTTPMethod.PATCH, path, func)
             return func
 
         return decorator
 
     def delete(self, path):
         def decorator(func):
-            self.add_route(HTTPMethod.DELETE, path, func)
+            self._add_route(HTTPMethod.DELETE, path, func)
             return func
 
         return decorator
